ed132492f34ba72bfa52f263b946793c9831f428
Vitalina Komashko <vitalina@gmail.com>
2019-07-25 15:25:03 -0700

    new function to read parameters from a YAML file and verify the validity and presence of mandatory parameters:

---
+++ R/parameters.R
@@ -0,0 +1,79 @@
+#' Read analysis parameters from a YAML file
+#'
+#' @param file_path Character string with a path to a YAML file.
+#'
+#' @return Named list with parameters for analysis.
+#'
+#' @export
+
+read_params <- function(file_path){
+  if (!file.exists(file_path)) {
+    stop("Attempting to read a yaml file. File `file_path` is not found.",
+         call. = FALSE)
+  } else {
+    params <- yaml::read_yaml(file_path)
+    # verify minimum required parameters
+    params <- verify_parameters(params)
+    return(params)
+  }
+}
+
+
+
+#' Verify validity of provided analysis parameters
+#'
+#' \code{verify_parameters} checks for the length of the list,
+#' presence of the mandatory parameters (\strong{primary_covs} and
+#' \strong{test_method}), allowed test method values, removes NULL parameters
+#' and checks "partner" parameters (for \strong{voom} it looks for
+#' \strong{voom_calc_norm_factors_method} and \strong{voom_normalize_method}).
+#'
+#' @param param_list List with parameters.
+#'
+#' @return List with parameters.
+
+verify_parameters <- function(param_list){
+
+  # check list length
+  if (length(param_list) == 0) {
+    stop("Expecting at least two parameters for analysis in `param_list`, ",
+         "however, zero provided.",
+         call. = FALSE)
+  }
+
+  # check if any of the parameters have been set to NULL and remove them
+  if ("NULL" %in% param_list) {
+    param_list[[which(param_list == "NULL")]] <- NULL
+  }
+
+  # check mandatory
+  min_needed <- c("primary_covs", "test_method")
+  for(i in min_needed){
+    if (!(i %in% names(param_list))) {
+      stop("Expecting mandatory parameter ", i, " in `param_list`, ",
+           "however, did not find it.",
+           call. = FALSE)
+    }
+  }
+
+  # check that test_method is set to either limma or lm
+  test_method_allowed <- c("limma", "lm")
+  if (param_list$test_method != "limma" | param_list$test_method != "lm") {
+    stop("Parameter `test_method` in `param_list` can be 'limma' or 'lm', ",
+         "however, found ", param_list$test_method, ".",
+         call. = FALSE)
+  }
+
+  # if voom is present verify that voom_calc_norm_factors_method and
+  # voom_normalize_method are also present
+  if ("voom" %in% names(param_list)) {
+    if (!(any(c("voom_calc_norm_factors_method", "voom_normalize_method") %in%
+              names(param_list)))) {
+      stop("Parameter `voom` is present, however, ",
+           "either `voom_calc_norm_factors_method` or `voom_normalize_method` ",
+           "are not present.",
+           call. = FALSE)
+    }
+  }
+  return(param_list)
+}
