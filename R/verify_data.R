a3a3a388e36f7b19dcc0dcb6ba5386a32e37b019
Vitalina Komashko <vitalina@gmail.com>
2019-07-31 14:43:54 -0700

    cleaned and added checks for the function check_expdata

--- R/verify_data.R
+++ R/verify_data.R
@@ -1,64 +1,122 @@
-check_expdata_easy <- function(expdata){
-  # check expression data format
-  # nessesary
-  needed <- c("data","gene.ann","sample.ann")
+#' Verify provided data and its structure.
+#'
+#' @param expdata Named list with the data components: data, gene_ann and
+#' sample_ann.
+#'
+#' @return Named list with the data components: data, gene_ann and sample_ann.
+#'
+#' @export
+check_expdata <- function(expdata){
 
-  indx <- needed %in% names(expdata)
-  if (!all(indx)) {
-    stop(paste0("NOT found ",
-                paste0(needed[!indx], collapse=" "),
-                sep = " "))
+  # verify that expdata is a list
+  if (class(expdata) != "list") {
+    stop("Expecting parameter `expdata` to be a list.",
+         call. = FALSE)
   }
 
-  # check class
+  # verify presence of all elements
+  needed <- c("data", "gene_ann", "sample_ann")
+  for(i in needed){
+    if (!(i %in% names(expdata))) {
+      stop("Expecting mandatory element ", i, " in `expdata`, ",
+           "however, did not find it.",
+           call. = FALSE)
+    }
+  }
+
+  # check class of data component
   if (!(is.matrix(expdata$data) |
         class(expdata$data) == "EList" |
         class(expdata$data) == "DGEList")) {
-    stop("expression data should be a matrix or EList")
+    stop("Expecting element `data` in `expdata` to be an instance of a class ",
+         "matrix or EList, or DGEList.",
+         call. = FALSE)
   }
 
-  if (!is.data.frame(expdata$gene.ann)) {
-    stop("gene annotation data should be a data.frame")
+  # verify that `data` has rownames
+  if (is.null(rownames(expdata$data))) {
+    stop("Expecting non-null row names in the element `data` ",
+         "of `expdata`.",
+         call. = FALSE)
   }
 
-  if (!is.data.frame(expdata$sample.ann)) {
-    stop("sample annotation data should be a data.frame")
+  # verify that `data` has column names
+  if (is.null(colnames(expdata$data))) {
+    stop("Expecting non-null column names in the element `data` ",
+         "of `expdata`.",
+         call. = FALSE)
   }
 
-  # check sample number
-  nsample <- dim(expdata$data)[2]
-  if (dim(expdata$sample.ann)[1] != nsample) {
-    stop("sample size does not match")
+  # check class of gene_ann component
+  if (!is.data.frame(expdata$gene_ann)) {
+    stop("Element `gene_ann` in `expdata` expected to be an instance of class ",
+         "data.frame.",
+         call. = FALSE)
   }
 
-  # check sample name
-  sampleid <- colnames(expdata$data)
-  if (!all(rownames(expdata$sample.ann) == sampleid)) {
-    stop("sample name not match")
+  # verify that the column names in `gene_ann` are set
+  if (is.null(colnames(expdata$gene_ann))){
+    stop("Expecting non-null column names in the element `gene_ann` ",
+         "of `expdata`.",
+         call. = FALSE)
   }
 
-
-  # check gene number
-  ngene <- dim(expdata$data)[1]
-  if (dim(expdata$gene.ann)[1] != ngene) {
-    stop("gene size does not match")
+  # check class of sample_ann component
+  if (!is.data.frame(expdata$sample_ann)) {
+    stop("Expecting element `sample_ann` in `expdata` to be an instance of ",
+         "class data.frame.",
+         call. = FALSE)
   }
 
-  # check gene name
-  geneid = rownames(expdata$data)
-  if (!all(rownames(expdata$gene.ann) == geneid)) {
-    stop("gene name does not match")
+  # verify that the column names in `gene_ann` are set
+  if (is.null(colnames(expdata$sample_ann))){
+    stop("Expecting non-null column names in the element `sample_ann` ",
+         "of `expdata`.",
+         call. = FALSE)
   }
 
-  return(TRUE)
+  # verify number of columns in `data` == number of rows in `sample_ann`
+  if (nrow(expdata$sample_ann) != ncol(expdata$data)) {
+    stop("Expecting the number of columns in the element `data` and ",
+         "the number of rows in the element `sample_ann` of `expdata` to be ",
+         "the same.",
+         call. = FALSE)
+  }
+
+  # verify match of row names in `sample_ann` and column names in `data`
+  if (all(rownames(expdata$sample_ann) != colnames(expdata$data))) {
+    stop("Expecting column names in the element `data` and ",
+         "row names in the element `sample_ann` of `expdata` to match.",
+         call. = FALSE)
+  }
+
+  # verify number of rows in `data` == number of rows in `gene_ann`
+  if (nrow(expdata$data) != nrow(expdata$gene_ann)) {
+    stop("Expecting the number of rows in the element `data` and ",
+         "the number of rows in the element `gene_ann` of `expdata` to be ",
+         "the same.",
+         call. = FALSE)
+  }
+
+  # verify match of row names in `gene_ann` and row names in `data`
+  if (all(rownames(expdata$gene_ann) != rownames(expdata$data))) {
+    stop("Expecting row names in the element `data` and ",
+         "row names in the element `gene_ann` of `expdata` to match.",
+         call. = FALSE)
+  }
+
+  return(expdata)
 }
 
 
+
+
+#' Verify
 sanity_check <- function(expdata,
                          PRIMARY_COV,
                          NEED_FOR_PLOT) {
   # checking expdata
-  if (!check_expdata_easy(expdata)){
+  if (!check_expdata(expdata)){
     stop()
   }
 
