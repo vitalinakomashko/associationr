b6b52f30fbf3d3d63daa87469e7112ff116dfddc
Vitalina Komashko <vitalina@gmail.com>
2019-08-13 13:45:37 -0700

    added test for the number of samples relative to the number of adjustment variables

--- tests/testthat/test-dataparam.R
+++ tests/testthat/test-dataparam.R
@@ -122,18 +122,28 @@ test_that("limma compatible values in primary_covs", {
 })
 
 
-# test_that("number of samples fewer than the number of adjust_covs", {
-#   expdata <- list()
-#   expdata$data <- matrix(rnorm(10*10), nrow = 10, ncol = 10)
-#   colnames(expdata$data) <- paste0("s", 1:10)
-#   rownames(expdata$data) <- paste0("g", 1:10)
-#   expdata$sample_ann <- data.frame(SAMPLE.ID = paste0("s",1:10),
-#                                    Group = factor(c(rep(1, 5), rep(2, 4), NA)),
-#                                    Cov1 = rnorm(10))
-#   expdata$gene_ann <- data.frame(Name = paste0("g", 1:10))
-#   params <- list(primary_covs = "Group",
-#                  adjust_covs = paste0("Cov", 1:11))
-#   expect_error(verify_input_data_parameters(expdata, params),
-#                paste0("The number of samples is fewer than the number of ",
-#                       "adjustment variables plus 2."))
-# })
+test_that("number of samples fewer than the number of adjust_covs", {
+  expdata <- list()
+  expdata$data <- matrix(rnorm(10*10), nrow = 10, ncol = 10)
+  colnames(expdata$data) <- paste0("s", 1:10)
+  rownames(expdata$data) <- paste0("g", 1:10)
+  expdata$sample_ann <- data.frame(SAMPLE.ID = paste0("s",1:10),
+                                   Group = factor(c(rep(1, 3), rep(2, 3),
+                                                    rep(NA, 4))),
+                                   Cov1 = rnorm(10),
+                                   Cov2 = rnorm(10, 2, 0.3),
+                                   Cov3 = rbinom(10, 2, 0.3),
+                                   Cov4 = rchisq(10, 2),
+                                   Cov5 = rnorm(10),
+                                   Cov6 = factor(1:10),
+                                   Cov7 = rexp(10),
+                                   Cov8 = rgeom(10, 0.2),
+                                   Age = sample(60:85, 10))
+  expdata$gene_ann <- data.frame(Name = paste0("g", 1:10))
+  params <- list(primary_covs = "Group",
+                 adjust_covs = paste0("Cov", 1:6),
+                 ignore_sample_size = TRUE)
+  expect_error(verify_input_data_parameters(expdata, params),
+               paste0("The number of samples is fewer than the number of ",
+                      "adjustment variables plus 2."))
+})
